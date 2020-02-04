2b2562a123a354a11914aeadaa1c20fd832c5b8b
Andrew Gray <andrew.paul.gray@gmail.com>
2020-02-04 19:31:23 +1100

    initial commit

---
+++ Core/RestAPI/FlowControl/SauceLabsFlowController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using SauceOps.Core.RestAPI.FlowControl.Base;
+using SauceOps.Core.Util;
+
+namespace SauceOps.Core.RestAPI.FlowControl {
+    internal class SauceLabsFlowController : FlowController {
+        public override void ControlFlow() {
+            while(TooManyTests()) {
+                Thread.Sleep(SauceryConstants.SAUCELABS_FLOW_WAIT);
+            }
+        }
+
+        protected override bool TooManyTests() {
+            //int maxParallelMacSessionsAllowed;  //Possible future use.
+            var json = GetJsonResponseForUser(SauceryConstants.ACCOUNT_CONCURRENCY_REQUEST);
+            //Console.WriteLine("Concurrency JSON: " + json);
+            var jsonStartIndex = json.IndexOf("\"remaining", StringComparison.Ordinal);
+
+            while(jsonStartIndex < 0)
+            {
+                json = GetJsonResponseForUser(SauceryConstants.ACCOUNT_CONCURRENCY_REQUEST);
+                jsonStartIndex = json.IndexOf("\"remaining", StringComparison.Ordinal);
+            }
+
+            var jsonEndIndex = json.Length - 3;
+            var remainingSection = ExtractJsonSegment(json, jsonStartIndex, jsonEndIndex);
+            var flowControl = SimpleJson.SimpleJson.DeserializeObject<FlowControl>(remainingSection);
+
+            return flowControl.remaining.overall <= 0;
+        }
+    }
+}
+/*
+ * Copyright Andrew Gray, Full Circle Solutions
+ * Date: 12th July 2014
+ * 
+ */
