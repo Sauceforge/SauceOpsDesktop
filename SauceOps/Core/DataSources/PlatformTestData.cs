7833d5945f0336485db2387a5db29aaf41cf1e1e
Andrew Gray <andrew.gray@coles.com.au>
2020-03-03 21:24:50 +1100

    restore for now

--- SauceOps/Core/DataSources/PlatformTestData.cs
+++ SauceOps/Core/DataSources/PlatformTestData.cs
@@ -10,17 +10,17 @@ namespace SauceOps.Core.DataSources
             {
                 ////Desktop Platforms
                 yield return new SaucePlatform("Windows 10", "chrome", "latest", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "chrome", "62", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "chrome", "61", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "firefox", "54", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "firefox", "53", "", "", "", "", "", "");
-                ////yield return new SaucePlatform("macOS 10.15", "safari", "13.0", "", "", "", "", "", "");
-                //yield return new SaucePlatform("macOS 10.13", "safari", "11.1", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "internet explorer", "11", "", "", "", "", "", "");
-                //yield return new SaucePlatform("Windows 10", "microsoftedge", "80", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "chrome", "62", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "chrome", "61", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "firefox", "54", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "firefox", "53", "", "", "", "", "", "");
+                //yield return new SaucePlatform("macOS 10.15", "safari", "13.0", "", "", "", "", "", "");
+                yield return new SaucePlatform("macOS 10.13", "safari", "11.1", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "internet explorer", "11", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "microsoftedge", "80", "", "", "", "", "", "");
 
                 //Mobile Platforms - TESTING ONLY AT THIS STAGE
-                yield return new SaucePlatform("","", "latest", "", "", "13.0", "", "iPhone XS Max Simulator", "portrait");
+                //yield return new SaucePlatform("","", "latest", "", "", "13.0", "", "iPhone XS Max Simulator", "portrait");
             }
         }
     }
