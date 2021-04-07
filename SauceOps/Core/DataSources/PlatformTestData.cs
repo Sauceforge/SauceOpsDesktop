aab58b872601885524bd4e855887c9815205da9b
Andrew Gray <andrew.gray@coles.com.au>
2021-04-08 09:43:52 +1000

    update platforms

--- SauceOps/Core/DataSources/PlatformTestData.cs
+++ SauceOps/Core/DataSources/PlatformTestData.cs
@@ -10,14 +10,14 @@ namespace SauceOps.Core.DataSources
             {
                 ////Desktop Platforms
                 yield return new SaucePlatform("Windows 10", "chrome", "latest", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "chrome", "62", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "chrome", "61", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "firefox", "54", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "firefox", "53", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "chrome", "88", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "chrome", "75", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "firefox", "87", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "firefox", "86", "", "", "", "", "", "");
                 //yield return new SaucePlatform("macOS 10.15", "safari", "13.0", "", "", "", "", "", "");
-                yield return new SaucePlatform("macOS 10.13", "safari", "11.1", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "internet explorer", "11", "", "", "", "", "", "");
-                yield return new SaucePlatform("Windows 10", "microsoftedge", "80", "", "", "", "", "", "");
+                yield return new SaucePlatform("macOS 10.15", "safari", "13.1", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "internet explorer", "11.285", "", "", "", "", "", "");
+                yield return new SaucePlatform("Windows 10", "microsoftedge", "89.0", "", "", "", "", "", "");
             }
         }
     }
